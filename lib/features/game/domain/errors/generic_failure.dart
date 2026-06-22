5f9384b10472c8bd929b1cb6b260b32ed504d971
AlbertoMonasterio <apmonasterio.22@est.ucab.edu.ve>
2026-06-21 21:14:39 -0400

    feat(game-flow): add EvaluateGameStateUseCase

--- lib/features/game/domain/errors/generic_failure.dart
+++ lib/features/game/domain/errors/generic_failure.dart
@@ -2,5 +2,5 @@ import 'failure.dart';
 
 /// Un error concreto para manejar fallos generales o excepciones inesperadas.
 class GenericFailure extends Failure {
-  const GenericFailure(String message) : super(message);
+  const GenericFailure(super.message);
 }
