74244bda46d254ad5dbc986ce12040c52c97b138
a-granadillo <agranadillo.21@est.ucab.edu.ve>
2026-07-16 11:20:53 -0400

    feat(aop): wire repository decorators into composition root

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app.dart';
+import 'core/aop/app_bloc_observer.dart';
 import 'core/database/hive_config.dart';
 import 'core/di/service_locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Bloc.observer = AppBlocObserver();
+
   await HiveConfig.initialise();
   await setupServiceLocator();
   runApp(const ArrowConMangoApp());
